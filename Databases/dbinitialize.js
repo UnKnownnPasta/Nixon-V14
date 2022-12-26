a36774c3ebd2e83875cdc8c361257f9f1e608d43
UnKnownnPasta <109729125+UnKnownnPasta@users.noreply.github.com>
2022-12-26 14:29:25 +0530

    Update dbinitialize.js

--- Databases/dbinitialize.js
+++ Databases/dbinitialize.js
@@ -6,15 +6,21 @@ const customBots = new Sequelize({
 	storage: './Databases/customBots.sqlite'
 })
 
-const normDB = new Sequelize({
+const karmaDB = new Sequelize({
 	dialect: 'sqlite',
     logging: false,
-	storage: './Databases/database.sqlite'
+	storage: './Databases/karmaDB.sqlite',
+})
+
+const classicDB = new Sequelize({
+	dialect: 'sqlite',
+    logging: false,
+	storage: './Databases/snipeafk.sqlite'
 })
 
 try {
     customBots.authenticate();
-    normDB.authenticate();
+    karmaDB.authenticate();
     console.log('(sequelize) Connection has been established successfully.');
   } catch (error) {
     console.error('(sequelize) Unable to connect to the database:', error);
@@ -30,10 +36,7 @@ customBots.define('Bots', {
         unique: true
     },
 	name: { type: DataTypes.STRING },
-	avatarurl: {
-		type: DataTypes.TEXT,
-		unique: true,   
-	},
+	avatarurl: { type: DataTypes.STRING },
     prefix: { type: DataTypes.STRING },
     test_cmd: { type: DataTypes.STRING },
     test_resp: { type: DataTypes.STRING },
@@ -41,19 +44,15 @@ customBots.define('Bots', {
     timestamps: false
 });
 
-customBots.define('CBProfile', {
+customBots.define('CBProfiles', {
     user_id: {
         type: DataTypes.INTEGER,
         unique: true
     },
     quoteCmd: { type: DataTypes.STRING },
     weatherCmd: { type: DataTypes.STRING },
-    bankCmd: { type: DataTypes.STRING },
-    bankBalance: { type: DataTypes.STRING },
     embedFeature: { type: DataTypes.STRING },
-    featureCmd: { type: DataTypes.STRING },
-    substFeature1: { type: DataTypes.STRING },
-    substFeature2: { type: DataTypes.STRING },
+    featureCmd: { type: DataTypes.STRING }
 }, {
     timestamps: false
 })
@@ -82,13 +81,67 @@ customBots.define('Cmds', {
     timestamps: false
 })
 
-module.exports = { customBots, normDB }
+karmaDB.define('Karmas', {
+    user_id: {
+        type: DataTypes.STRING,
+        unique: true
+    }, 
+    balance: { type: DataTypes.INTEGER },
+    prestige: { type: DataTypes.INTEGER },
+    multiplier: { type: DataTypes.FLOAT },
+    prestigePercent: { type: DataTypes.FLOAT },
+    prestigeReq: { type: DataTypes.FLOAT },
+    prestigeEmojis: { type: DataTypes.STRING, defaultValue: '◾◽' }
+}, {
+    timestamps: false
+})
+
+classicDB.define('editSnipes', {
+    user_id: {
+        type: DataTypes.STRING,
+        unique: true
+    }, 
+    content: { type: DataTypes.STRING },
+    username: { type: DataTypes.STRING },
+    avatarurl: { type: DataTypes.STRING },
+    channelId: { type: DataTypes.STRING }
+}, {
+    timestamps: false
+})
+
+classicDB.define('snipes', {
+    user_id: {
+        type: DataTypes.STRING,
+        unique: true
+    }, 
+    content: { type: DataTypes.STRING },
+    username: { type: DataTypes.STRING },
+    avatarurl: { type: DataTypes.STRING },
+    channelId: { type: DataTypes.STRING }
+}, {
+    timestamps: false
+})
+
+classicDB.define('afks', {
+    user_id: {
+        type: DataTypes.STRING,
+        unique: true
+    }, 
+    message: { type: DataTypes.STRING },
+    status: { type: DataTypes.BOOLEAN }
+}, {
+    timestamps: false
+})
 
 if (1 == 1) {
     (async () => {
-        await customBots.sync({ force: true });
-        console.log('(sequelize) Synced DB [1/2]')
-        await normDB.sync({ force: true });
-        console.log('(sequelize) Synced DB [2/2]')
+        await customBots.sync();
+        console.log('(sequelize) Synced DB [1/3]')
+        await karmaDB.sync();
+        console.log('(sequelize) Synced DB [2/3]')
+        await classicDB.sync()
+        console.log('(sequelize) Synced DB [3/3]')
     })();
 }
+
+module.exports = { karmaDB, customBots, classicDB }
